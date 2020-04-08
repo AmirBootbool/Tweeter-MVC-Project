d3427fdbb26dd2c6561e80ff539bf3f97d07a16c
AmirBootbool <amir.bootbool@gmail,com>
2020-04-08 16:46:57 +0300

    start twit logic file

---
+++ logic.js
@@ -0,0 +1,9 @@
+// An array of posts - this should be private. Use the dummy data below to get started
+// A variable postIdCounter to count the number of total posts
+// A variable commentIdCounter to count the number of total comments
+// A getPosts function that returns the posts array
+// An addPost function that receives some text and adds a post object to posts
+// Each object should have three properties: id, text, and comments
+// You should generate the next id correctly for each post: "p4", "p5", ...
+// The comments array should be empty initially
+// A removePost function that receives a postID and removes the relevant post from posts
