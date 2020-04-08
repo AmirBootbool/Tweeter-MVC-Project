6b42d334e7b4924f36cd01602cb40b5cba8f5fe2
AmirBootbool <amir.bootbool@gmail,com>
2020-04-08 17:34:32 +0300

    logic 1st section in progress.

--- logic.js
+++ logic.js
@@ -1,9 +1,34 @@
-// An array of posts - this should be private. Use the dummy data below to get started
-// A variable postIdCounter to count the number of total posts
-// A variable commentIdCounter to count the number of total comments
-// A getPosts function that returns the posts array
-// An addPost function that receives some text and adds a post object to posts
-// Each object should have three properties: id, text, and comments
+
+let _postsList = []
+// _postsList.push("dummy post")
+let postIdCounter = _postsList.length
+// console.log(_postsList, postIdCounter)
+
+// let commentIdCounter = will count comments (obj items?)
+
+const getPosts = function (){
+    return _postsList
+}
+
+// console.log(getPosts())  test function return the array
+let counter = 1
+const addPost = function (string){
+    const add =  function () {
+        counter += 1;
+      }
+    _postsList.push({   id:"p"+counter,
+                        text: string ,
+                        comments: "comments" }) // push object with key set to number of total
+                        add() 
+}
+addPost("test push to list of objects")
+addPost("2nd push to list of objects")
+addPost("3rd push to list of objects")
+
+
+console.log(getPosts())
+
+
 // You should generate the next id correctly for each post: "p4", "p5", ...
 // The comments array should be empty initially
 // A removePost function that receives a postID and removes the relevant post from posts
