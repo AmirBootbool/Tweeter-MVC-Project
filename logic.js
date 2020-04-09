4e313488a2e134e2a990ff791b7f06f69dcfdd9b
AmirBootbool <amir.bootbool@gmail,com>
2020-04-09 11:44:34 +0300

    tweeter logic part 1 done and running.

--- logic.js
+++ logic.js
@@ -1,34 +1,74 @@
 
+
+// ########## TWEETER LOGIC ##########
+
 let _postsList = []
 // _postsList.push("dummy post")
 let postIdCounter = _postsList.length
 // console.log(_postsList, postIdCounter)
-
-// let commentIdCounter = will count comments (obj items?)
 
 const getPosts = function (){
     return _postsList
 }
 
 // console.log(getPosts())  test function return the array
-let counter = 1
+
+
+// adding posts and comments to DB with post number 
+
+let counter = 1  // set counter ouside of target function to prevent it from loosing count each time the function is called
+
 const addPost = function (string){
     const add =  function () {
         counter += 1;
       }
     _postsList.push({   id:"p"+counter,
                         text: string ,
-                        comments: "comments" }) // push object with key set to number of total
+                        comments:[] })
                         add() 
 }
-addPost("test push to list of objects")
-addPost("2nd push to list of objects")
-addPost("3rd push to list of objects")
+
+// console.log(getPosts())
+
+// function removePost(array, postIndex) { 
+//     return array.filter(function(ele){
+//         return ele != postIndex; });
+// }
+
+// ### helper remove function ###
+
+     // a method to remove an element from array by its value- using  filter to bring back only values that doenst match the targeted item to be removed.
+
+    //  function arrayRemove(arr, value) { 
+    //      return arr.filter(function(ele){
+    //           return ele != value; });}
+
+    // push some new posts inside the list
+
+    addPost("1st dummy post")
+    addPost("2nd dummy post")
+    addPost("3rd dummy post")
+
+    console.log("before removing", getPosts())  // print post DB
+
+const searchAndDeletePost = function (_postsList, postId){
+        for (let item of _postsList){
+            // console.log(item)
+            let key = item.id
+            // console.log(key)
+        if (key == postId){
+            let index = _postsList.indexOf(item)
+            // console.log(index)
+            _postsList.splice(index, 1)
+        }
+    }
+}
+searchAndDeletePost(_postsList, "p3")
+console.log("after removing", getPosts())
 
 
-console.log(getPosts())
+
+              
 
 
-// You should generate the next id correctly for each post: "p4", "p5", ...
-// The comments array should be empty initially
-// A removePost function that receives a postID and removes the relevant post from posts
+
